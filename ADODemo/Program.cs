30a9140bfcc75cf546573a4ff8abd21bc953e37c
varsha Gunjal <varshashingote01@gmail.com>
2023-03-07 21:01:09 +0530

    [varsha]updateEmp

--- ADODemo/Program.cs
+++ ADODemo/Program.cs
@@ -13,6 +13,11 @@ namespace ADODemo
         {
    
             EmployeeRepository model = new EmployeeRepository();
+            model.Name = 'Vishu';
+            model.id = 7;
+            model.Basic_pay = 1234;
+
+
             EmployeeRepository.AddEmployee(model);
 
             Console.ReadLine();
